3f2354c17396fd6060b844884c06eccca46a385b
Andrzej Jozwiak <andrzej.jozwiak@gmail.com>
2016-12-29 01:11:00 +0100

    [day1][fix] Incorrect direction result for South (Right should give West, Left should give East -- and was otherwise)

--- aoc16/src/main/kotlin/io/github/ajoz/workshop/day1/Day1Puzzle.kt
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day1/Day1Puzzle.kt
@@ -51,8 +51,8 @@ fun Direction.next(forAn: Instruction) = when {
     this is East && forAn is Left -> North()
     this is West && forAn is Right -> North()
     this is West && forAn is Left -> South()
-    this is South && forAn is Right -> East()
-    this is South && forAn is Left -> West()
+    this is South && forAn is Right -> West()
+    this is South && forAn is Left -> East()
     else -> throw IllegalArgumentException("Unknown direction: $this and instruction: $forAn combination")
 }
 
@@ -72,15 +72,19 @@ fun getShortestPathToDestinationLength(instructions: String): Int {
             .fold(PartialSolution(North(), start), {
                 solution, instruction ->
                 val nextDirection = solution.direction.next(forAn = instruction)
-                PartialSolution(nextDirection, nextCoordinates(solution.coordinates, nextDirection, instruction))
+                val partial = PartialSolution(nextDirection, nextCoordinates(solution.coordinates, nextDirection, instruction))
+                println(nextCoordinates(solution.coordinates, nextDirection, instruction))
+                partial
             }).coordinates
 
     return Math.abs(start.x - stop.x) + Math.abs(start.y - stop.y)
 }
 
 fun main(args: Array<String>) {
-    println(getShortestPathToDestinationLength("R2, L3"))
-    println(getShortestPathToDestinationLength("R2, R2, R2"))
-    println(getShortestPathToDestinationLength("R5, L5, R5, R3"))
+//    println(getShortestPathToDestinationLength("R2, L3"))
+//    println(getShortestPathToDestinationLength("R2, R2, R2"))
+//    println(getShortestPathToDestinationLength("R5, L5, R5, R3"))
+//    println(getShortestPathToDestinationLength("R2, R2, R2, R2"))
+//    println(getShortestPathToDestinationLength("R8, R4, R4, R8"))
     println(getShortestPathToDestinationLength("L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, L4, L4, L3, L4, L3, L5, R2, R4, L2"))
 }

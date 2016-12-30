f34928db0f64ee618e9d9bb91283dbb8a6ad9e53
Andrzej Jozwiak <andrzej.jozwiak@gmail.com>
2016-12-30 23:46:33 +0100

    [day1][dev] Initial workshop file structure (implemented on day puzzle). Moving solution from Puzzle file to Solution file.

---
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day1/Day1Part1Puzzle.kt
@@ -0,0 +1,23 @@
+package io.github.ajoz.workshop.day1
+
+/**
+ * Day 1: No Time for a Taxicab
+ *
+ * You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you can get
+ * - the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody had time to work
+ * them out further.
+ *
+ * The Document indicates that you should start at the given coordinates (where you just landed) and face North. Then,
+ * follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of
+ * blocks, ending at a new intersection.
+ *
+ * There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the
+ * destination. Given that you can only walk on the street grid of the city, how far is the shortest path to the
+ * destination?
+ *
+ * £http://adventofcode.com/2016/day/1
+ */
+
+fun getDay1Part1Solution(instructions: String): Int {
+    TODO()
+}
---
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day1/Day1Part2Puzzle.kt
@@ -0,0 +1,12 @@
+package io.github.ajoz.workshop.day1
+
+/**
+ * Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the
+ * first location you visit twice. For example, if your instructions are R8, R4, R4, R8, the first location you visit
+ * twice is 4 blocks away, due East.
+ *
+ * http://adventofcode.com/2016/day/1
+ */
+fun getDay1Part2Solution(instructions: String): Int {
+    TODO()
+}

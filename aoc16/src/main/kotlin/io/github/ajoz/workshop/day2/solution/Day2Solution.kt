f88f3faabbc9045376b5c2ce9d0e22581266caf1
Andrzej Jozwiak <andrzej.jozwiak@tomtom.com>
2017-01-03 16:58:37 +0100

    [day2][dev] Exploring a DSL for FSM

--- aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
@@ -1,7 +1,5 @@
 package io.github.ajoz.workshop.day2.solution
 
-import io.github.ajoz.workshop.sequences.scan
-
 /**
  * You arrive at Easter Bunny Headquarters under cover of darkness. However, you left in such a rush that you forgot to
  * use the bathroom! Fancy office buildings like this one usually have keypad locks on their bathrooms, so you search
@@ -45,42 +43,43 @@ import io.github.ajoz.workshop.sequences.scan
  * We can see the solution to this puzzle in terms of a deterministic FSM (finite state machine). An input alphabet of
  * a FSM consists of a set of symbols:
  */
-sealed class Symbol {
-    class Up : Symbol()
-    class Left : Symbol()
-    class Right : Symbol()
-    class Down : Symbol()
-
-    override fun toString() = when (this) {
-        is Symbol.Up -> "Up"
-        is Symbol.Down -> "Down"
-        is Symbol.Left -> "Left"
-        is Symbol.Right -> "Right"
-    }
+enum class Symbol {
+    Up,
+    Left,
+    Right,
+    Down;
 }
 
 val Char.symbol: Symbol?
     get() = when (this) {
-        'U' -> Symbol.Up()
-        'D' -> Symbol.Down()
-        'L' -> Symbol.Left()
-        'R' -> Symbol.Right()
+        'U' -> Symbol.Up
+        'D' -> Symbol.Down
+        'L' -> Symbol.Left
+        'R' -> Symbol.Right
         else -> null
     }
 
-sealed class Part1State(val value: Char) {
-    class Key1 : Part1State('1')
-    class Key2 : Part1State('2')
-    class Key3 : Part1State('3')
-    class Key4 : Part1State('4')
-    class Key5 : Part1State('5')
-    class Key6 : Part1State('6')
-    class Key7 : Part1State('7')
-    class Key8 : Part1State('8')
-    class Key9 : Part1State('9')
+enum class SimpleKeypad(val value: Char) {
+    Key1('1'),
+    Key2('2'),
+    Key3('3'),
+    Key4('4'),
+    Key5('5'),
+    Key6('6'),
+    Key7('7'),
+    Key8('8'),
+    Key9('9')
 }
 
+data class Match<out T, out V>(val state: T, val symbol: V)
+data class Transition<out T, out V>(val match: Match<T, V>, val state: T)
 
+infix fun <T, V> T.with(value: V) = Match(this, value)
+infix fun <T, V> Match<T, V>.transitionsTo(state: T) = Transition(this, state)
+
+fun <T, V> transitions(vararg transtions: Transition<T, V>) {
+
+}
 
 fun main(args: Array<String>) {
     val test = """
@@ -89,6 +88,18 @@ fun main(args: Array<String>) {
                 |LURDL
                 |UUUUD
                """.trimMargin()
+
+    transitions(
+            SimpleKeypad.Key1 with Symbol.Up transitionsTo SimpleKeypad.Key1,
+            SimpleKeypad.Key2 with Symbol.Up transitionsTo SimpleKeypad.Key2,
+            SimpleKeypad.Key3 with Symbol.Up transitionsTo SimpleKeypad.Key3,
+            SimpleKeypad.Key4 with Symbol.Up transitionsTo SimpleKeypad.Key1,
+            SimpleKeypad.Key5 with Symbol.Up transitionsTo SimpleKeypad.Key2,
+            SimpleKeypad.Key6 with Symbol.Up transitionsTo SimpleKeypad.Key3,
+            SimpleKeypad.Key7 with Symbol.Up transitionsTo SimpleKeypad.Key4,
+            SimpleKeypad.Key8 with Symbol.Up transitionsTo SimpleKeypad.Key5,
+            SimpleKeypad.Key9 with Symbol.Up transitionsTo SimpleKeypad.Key6
+    )
 
 
 }

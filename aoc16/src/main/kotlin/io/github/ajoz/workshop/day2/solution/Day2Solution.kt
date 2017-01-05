e08860337932ae947822bec3861cb6bdd6d7b7be
Andrzej Jozwiak <andrzej.jozwiak@gmail.com>
2017-01-05 23:58:52 +0100

    [day2][part1] Moving functionality from extension functions, Going aways from raw Collections, Simple immutable FSM implementation.

--- aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
@@ -8,17 +8,42 @@ package io.github.ajoz.workshop.day2.solution
 data class State<T>(val value: T) {
     override fun toString() = "State($value)"
 
-    infix fun or(other: State<T>) = setOf(this, other)
+    infix fun or(other: State<T>) = States(setOf(this, other))
 
     infix fun <V> after(symbol: Symbol<V>) = Match(this, symbol)
 
     infix fun <V> after(symbols: Set<Symbol<V>>) = symbols.map { Match(this, it) }.toSet()
 }
 
+data class States<T>(val values: Set<State<T>>) {
+    infix fun or(state: State<T>) = States(values + state)
+
+    infix fun <V> cyclesAfter(symbol: Symbol<V>): Transitions<T, V> {
+        val transitions = values
+                .map { state -> Match(state, symbol) }
+                .map { match -> Pair(match, match.state) }
+                .toMap()
+        return Transitions(transitions)
+    }
+
+    infix fun <V> cyclesAfter(symbols: Set<Symbol<V>>): Transitions<T, V> {
+        val transitions = symbols
+                .map { symbol -> this.cyclesAfter(symbol) }
+                .flatMap { it.values.toList() }
+                .toMap()
+
+        return Transitions(transitions)
+    }
+}
+
 data class Symbol<T>(val value: T) {
     override fun toString() = "Symbol($value)"
 
-    infix fun or(other: Symbol<T>) = setOf(this, other)
+    infix fun or(other: Symbol<T>) = Symbols(setOf(this, other))
+}
+
+data class Symbols<T>(val values: Set<Symbol<T>>) {
+    infix fun or(symbol: Symbol<T>) = Symbols(values + symbol)
 }
 
 data class Match<T, V>(val state: State<T>, val symbol: Symbol<V>) {
@@ -29,68 +54,65 @@ data class Match<T, V>(val state: State<T>, val symbol: Symbol<V>) {
 
 data class Transitions<T, V>(val values: Map<Match<T, V>, State<T>>)
 
-infix fun <T> Set<State<T>>.or(other: State<T>) = this + other
-infix fun <T> Set<Symbol<T>>.or(other: Symbol<T>) = this + other
+fun <T, V> transitions(vararg transitions: Transitions<T, V>) {
 
-infix fun <T, V> Set<State<T>>.cyclesAfter(other: Symbol<V>): Transitions<T, V> {
-    val transitions = this
-            .map { Match(it, other) }
-            .map { Pair(it, it.state) }
-            .toMap()
-    return Transitions(transitions)
 }
 
-infix fun <T, V> Set<State<T>>.cyclesAfter(other: Set<Symbol<V>>): Transitions<T, V> {
-    val transitions = other
-            .map { symbol -> this.cyclesAfter(symbol) }
-            .flatMap { it.values.toList() }
-            .toMap()
+class FSM<T, V>(val state: State<T>, transition: (State<T>, Symbol<V>) -> State<T>) {
+    val transition = transition
 
-    return Transitions(transitions)
+    fun next(symbol: Symbol<V>): FSM<T, V> {
+        val nextState = transition(state, symbol)
+        return FSM(nextState, transition)
+    }
 }
-
-fun <T, V> matchOf(state: T, symbol: V) = Match(State(state), Symbol(symbol))
 
 val Char.symbol: Symbol<Char>
     get() = Symbol(this)
 
+fun <T, V> accept(state: State<T>, transition: (State<T>, Symbol<V>) -> State<T>) {
+
+}
 
 fun main(args: Array<String>) {
-    (State(1) or State(2) or State(3)) cyclesAfter Symbol('U')
-    (State(1) or State(4) or State(7)) cyclesAfter Symbol('L')
-    (State(3) or State(6) or State(9)) cyclesAfter Symbol('R')
-    (State(7) or State(8) or State(9)) cyclesAfter Symbol('D')
+    val transitions = transitions(
+            (State(1) or State(2) or State(3)) cyclesAfter Symbol('U'),
+            (State(1) or State(4) or State(7)) cyclesAfter Symbol('L'),
+            (State(3) or State(6) or State(9)) cyclesAfter Symbol('R'),
+            (State(7) or State(8) or State(9)) cyclesAfter Symbol('D'),
 
-    State(1) after Symbol('R') transitionsTo State(2)
-    State(1) after Symbol('D') transitionsTo State(4)
+            State(1) after Symbol('R') transitionsTo State(2),
+            State(1) after Symbol('D') transitionsTo State(4),
 
-    State(2) after Symbol('L') transitionsTo State(1)
-    State(2) after Symbol('R') transitionsTo State(3)
-    State(2) after Symbol('D') transitionsTo State(5)
+            State(2) after Symbol('L') transitionsTo State(1),
+            State(2) after Symbol('R') transitionsTo State(3),
+            State(2) after Symbol('D') transitionsTo State(5),
 
-    State(3) after Symbol('L') transitionsTo State(2)
-    State(3) after Symbol('D') transitionsTo State(6)
+            State(3) after Symbol('L') transitionsTo State(2),
+            State(3) after Symbol('D') transitionsTo State(6),
 
-    State(4) after Symbol('U') transitionsTo State(1)
-    State(4) after Symbol('R') transitionsTo State(5)
-    State(4) after Symbol('D') transitionsTo State(7)
+            State(4) after Symbol('U') transitionsTo State(1),
+            State(4) after Symbol('R') transitionsTo State(5),
+            State(4) after Symbol('D') transitionsTo State(7),
 
-    State(5) after Symbol('U') transitionsTo State(2)
-    State(5) after Symbol('L') transitionsTo State(4)
-    State(5) after Symbol('R') transitionsTo State(6)
-    State(5) after Symbol('D') transitionsTo State(8)
+            State(5) after Symbol('U') transitionsTo State(2),
+            State(5) after Symbol('L') transitionsTo State(4),
+            State(5) after Symbol('R') transitionsTo State(6),
+            State(5) after Symbol('D') transitionsTo State(8),
 
-    State(6) after Symbol('U') transitionsTo State(3)
-    State(6) after Symbol('L') transitionsTo State(5)
-    State(6) after Symbol('D') transitionsTo State(9)
+            State(6) after Symbol('U') transitionsTo State(3),
+            State(6) after Symbol('L') transitionsTo State(5),
+            State(6) after Symbol('D') transitionsTo State(9),
 
-    State(7) after Symbol('U') transitionsTo State(4)
-    State(7) after Symbol('R') transitionsTo State(8)
+            State(7) after Symbol('U') transitionsTo State(4),
+            State(7) after Symbol('R') transitionsTo State(8),
 
-    State(8) after Symbol('U') transitionsTo State(5)
-    State(8) after Symbol('L') transitionsTo State(7)
-    State(8) after Symbol('R') transitionsTo State(9)
+            State(8) after Symbol('U') transitionsTo State(5),
+            State(8) after Symbol('L') transitionsTo State(7),
+            State(8) after Symbol('R') transitionsTo State(9),
 
-    State(9) after Symbol('U') transitionsTo State(6)
-    State(9) after Symbol('L') transitionsTo State(8)
+            State(9) after Symbol('U') transitionsTo State(6),
+            State(9) after Symbol('L') transitionsTo State(8)
+    )
+
 }

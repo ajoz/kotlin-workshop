6122055226feeee8c599e38dec1456342f6f4c8d
Andrzej Jozwiak <andrzej.jozwiak@gmail.com>
2017-01-06 01:11:59 +0100

    [day2] Checking if correct structures are used for working with States and Symbols.

--- aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
@@ -54,8 +54,11 @@ data class Match<T, V>(val state: State<T>, val symbol: Symbol<V>) {
 
 data class Transitions<T, V>(val values: Map<Match<T, V>, State<T>>)
 
-fun <T, V> transitions(vararg transitions: Transitions<T, V>) {
-
+fun <T, V> transitions(vararg transitions: Transitions<T, V>): (State<T>, Symbol<V>) -> State<T> {
+//    val t = transitions.map { it.values. }
+    return fun (st: State<T>, sy: Symbol<V>): State<T> {
+        return st
+    }
 }
 
 class FSM<T, V>(val state: State<T>, transition: (State<T>, Symbol<V>) -> State<T>) {
@@ -70,49 +73,50 @@ class FSM<T, V>(val state: State<T>, transition: (State<T>, Symbol<V>) -> State<
 val Char.symbol: Symbol<Char>
     get() = Symbol(this)
 
-fun <T, V> accept(state: State<T>, transition: (State<T>, Symbol<V>) -> State<T>) {
+val String.head: Char
+    get() = get(0)
 
+val String.tail: String
+    get() = drop(1)
+
+tailrec fun accept(fsm: FSM<Int, Char>, instruction: String): FSM<Int, Char> = when {
+    instruction.isEmpty() -> fsm
+    else -> accept(fsm.next(instruction.head.symbol), instruction.tail)
 }
 
 fun main(args: Array<String>) {
-    val transitions = transitions(
+    val t = transitions(
             (State(1) or State(2) or State(3)) cyclesAfter Symbol('U'),
             (State(1) or State(4) or State(7)) cyclesAfter Symbol('L'),
             (State(3) or State(6) or State(9)) cyclesAfter Symbol('R'),
             (State(7) or State(8) or State(9)) cyclesAfter Symbol('D'),
-
             State(1) after Symbol('R') transitionsTo State(2),
             State(1) after Symbol('D') transitionsTo State(4),
-
             State(2) after Symbol('L') transitionsTo State(1),
             State(2) after Symbol('R') transitionsTo State(3),
             State(2) after Symbol('D') transitionsTo State(5),
-
             State(3) after Symbol('L') transitionsTo State(2),
             State(3) after Symbol('D') transitionsTo State(6),
-
             State(4) after Symbol('U') transitionsTo State(1),
             State(4) after Symbol('R') transitionsTo State(5),
             State(4) after Symbol('D') transitionsTo State(7),
-
             State(5) after Symbol('U') transitionsTo State(2),
             State(5) after Symbol('L') transitionsTo State(4),
             State(5) after Symbol('R') transitionsTo State(6),
             State(5) after Symbol('D') transitionsTo State(8),
-
             State(6) after Symbol('U') transitionsTo State(3),
             State(6) after Symbol('L') transitionsTo State(5),
             State(6) after Symbol('D') transitionsTo State(9),
-
             State(7) after Symbol('U') transitionsTo State(4),
             State(7) after Symbol('R') transitionsTo State(8),
-
             State(8) after Symbol('U') transitionsTo State(5),
             State(8) after Symbol('L') transitionsTo State(7),
             State(8) after Symbol('R') transitionsTo State(9),
-
             State(9) after Symbol('U') transitionsTo State(6),
             State(9) after Symbol('L') transitionsTo State(8)
     )
 
+    val fsm = FSM(State(5), t)
+    val result = accept(fsm, "ULL")
+    println(result)
 }

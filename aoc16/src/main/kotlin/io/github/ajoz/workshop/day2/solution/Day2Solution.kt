89c2303ff7b5beb3d79dc89d4909d1d22c21a823
Andrzej Jozwiak <andrzej.jozwiak@gmail.com>
2017-01-08 23:28:18 +0100

    [day2][part2] Transitions for part2 of the puzzle defined, Missing documentation for SymbolSet, Tests for Part2 (both puzzle and solution).

--- aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/day2/solution/Day2Solution.kt
@@ -28,9 +28,9 @@ import io.github.ajoz.workshop.strings.tail
 val Char.symbol: Symbol<Char>
     get() = Symbol(this)
 
-tailrec fun accept(fsm: FSM<Int, Char>, instruction: String): FSM<Int, Char> = when {
+tailrec fun acceptIntChar(fsm: FSM<Int, Char>, instruction: String): FSM<Int, Char> = when {
     instruction.isEmpty() -> fsm
-    else -> accept(fsm.accept(instruction.head.symbol), instruction.tail)
+    else -> acceptIntChar(fsm.accept(instruction.head.symbol), instruction.tail)
 }
 
 val day2part1Transition = transitions(
@@ -67,10 +67,65 @@ val day2part1Transition = transitions(
 fun getPart1BathroomAccessCode(instructions: String): Int {
     val fsm = FSM(State(5), day2part1Transition)
     return instructions.splitToSequence(delimiters = '\n')
-            .scan(fsm, ::accept)
+            .scan(fsm, ::acceptIntChar)
             .map { fsm -> fsm.state.value }
             .fold("") {
                 str, value ->
                 str + value
             }.toInt()
+}
+
+val day2part2Transition = transitions(
+        State('1') or State('2') or State('5') or State('A') or State('D') cyclesAfter Symbol('L'),
+        State('1') or State('4') or State('9') or State('C') or State('D') cyclesAfter Symbol('R'),
+        State('5') or State('2') or State('1') or State('4') or State('9') cyclesAfter Symbol('U'),
+        State('5') or State('A') or State('D') or State('C') or State('9') cyclesAfter Symbol('D'),
+        State('1') after Symbol('D') transitionsTo State('3'),
+        State('2') after Symbol('D') transitionsTo State('6'),
+        State('4') after Symbol('D') transitionsTo State('8'),
+        State('2') after Symbol('R') transitionsTo State('3'),
+        State('4') after Symbol('L') transitionsTo State('3'),
+        State('5') after Symbol('R') transitionsTo State('6'),
+        State('9') after Symbol('L') transitionsTo State('8'),
+        State('A') after Symbol('U') transitionsTo State('6'),
+        State('A') after Symbol('R') transitionsTo State('B'),
+        State('C') after Symbol('U') transitionsTo State('8'),
+        State('C') after Symbol('L') transitionsTo State('B'),
+        State('D') after Symbol('U') transitionsTo State('B'),
+        State('3') after Symbol('U') transitionsTo State('1'),
+        State('3') after Symbol('L') transitionsTo State('2'),
+        State('3') after Symbol('R') transitionsTo State('4'),
+        State('3') after Symbol('D') transitionsTo State('7'),
+        State('6') after Symbol('U') transitionsTo State('2'),
+        State('6') after Symbol('L') transitionsTo State('5'),
+        State('6') after Symbol('R') transitionsTo State('7'),
+        State('6') after Symbol('D') transitionsTo State('A'),
+        State('7') after Symbol('U') transitionsTo State('3'),
+        State('7') after Symbol('L') transitionsTo State('6'),
+        State('7') after Symbol('R') transitionsTo State('8'),
+        State('7') after Symbol('D') transitionsTo State('B'),
+        State('8') after Symbol('U') transitionsTo State('4'),
+        State('8') after Symbol('L') transitionsTo State('7'),
+        State('8') after Symbol('R') transitionsTo State('9'),
+        State('8') after Symbol('D') transitionsTo State('C'),
+        State('B') after Symbol('U') transitionsTo State('7'),
+        State('B') after Symbol('L') transitionsTo State('A'),
+        State('B') after Symbol('R') transitionsTo State('C'),
+        State('B') after Symbol('D') transitionsTo State('D')
+)
+
+tailrec fun acceptCharChar(fsm: FSM<Char, Char>, instruction: String): FSM<Char, Char> = when {
+    instruction.isEmpty() -> fsm
+    else -> acceptCharChar(fsm.accept(instruction.head.symbol), instruction.tail)
+}
+
+fun getPart2BathroomAccessCode(instructions: String): String {
+    val fsm = FSM(State('5'), day2part2Transition)
+    return instructions.splitToSequence(delimiters = '\n')
+            .scan(fsm, ::acceptCharChar)
+            .map { fsm -> fsm.state.value }
+            .fold("") {
+                str, value ->
+                str + value
+            }
 }
--- aoc16/src/main/kotlin/io/github/ajoz/workshop/fsm/SymbolSet.kt
+++ aoc16/src/main/kotlin/io/github/ajoz/workshop/fsm/SymbolSet.kt
@@ -1,11 +1,17 @@
 package io.github.ajoz.workshop.fsm
 
 /**
- *
+ * Contains a set of [Symbol] this allows to construct a small DSL for creating list of possible transition for the FSM.
  */
 data class SymbolSet<T>(val values: Set<Symbol<T>>) {
     /**
+     * Creates a [SymbolSet] from this set and a given [Symbol]. Very useful when chaining more symbols then two. This
+     * method allows us to write:
      *
+     * Symbol('U') or Symbol('D')
+     *
+     * Thanks to such notation we can express that a transition from one State to another State can happen after several
+     * different Symbols.
      */
     infix fun or(symbol: Symbol<T>) = SymbolSet(values + symbol)
 }
